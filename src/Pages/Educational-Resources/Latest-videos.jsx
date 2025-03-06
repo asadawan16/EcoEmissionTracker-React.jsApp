780e1a548a8d267256fc1c0f448bb26ca57e641b
asadawan16 <asadawan16900@gmail.com>
2025-03-06 16:06:42 +0500

    video links added

--- src/Pages/Educational-Resources/Latest-videos.jsx
+++ src/Pages/Educational-Resources/Latest-videos.jsx
@@ -1,35 +1,45 @@
 import React from "react";
 import "./Latest-videos.css";
+
 const Latestvideos = () => {
   return (
     <div className="latest-videos">
       <div className="container">
         <h2>Latest Videos</h2>
         <div className="videos">
-  <iframe
-    src="https://www.youtube.com/embed/3cmB9dDFtNU"
-    frameborder="0"
-    allowfullscreen
-    height={300}
-    width={300}
-  ></iframe>
-  <iframe
-    src="https://www.youtube.com/embed/FSJmJ9nx7FE"
-    frameborder="0"
-    allowfullscreen
-  ></iframe>
-  <iframe
-    src="https://www.youtube.com/embed/5CC7oggo_NM"
-    frameborder="0"
-    allowfullscreen
-  ></iframe>
-  <iframe
-    src="https://www.youtube.com/embed/gUuGEw1_09E"
-    frameborder="0"
-    allowfullscreen
-  ></iframe>
-</div>
-
+          <div className="video-container">
+            <iframe
+              src="https://www.youtube.com/embed/xkG8i_4mdCM"
+              frameBorder="0"
+              allowFullScreen
+              title="Video 1"
+            ></iframe>
+          </div>
+          <div className="video-container">
+            <iframe
+              src="https://www.youtube.com/embed/eJHEMFd61bs"
+              frameBorder="0"
+              allowFullScreen
+              title="Video 2"
+            ></iframe>
+          </div>
+          <div className="video-container">
+            <iframe
+              src="https://www.youtube.com/embed/q8VMsHKcywg"
+              frameBorder="0"
+              allowFullScreen
+              title="Video 3"
+            ></iframe>
+          </div>
+          <div className="video-container">
+            <iframe
+              src="https://www.youtube.com/embed/myZAvqqp9Jc"
+              frameBorder="0"
+              allowFullScreen
+              title="Video 4"
+            ></iframe>
+          </div>
+        </div>
       </div>
     </div>
   );
